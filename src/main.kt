ed009ca27bbce60a484cc6fd460f341ac71dba97
Fernanda Alves <fernandaalvesmonteiro7@gmail.com>
2021-02-11 23:58:50 -0300

    Adicionando classe de Gerente

--- src/main.kt
+++ src/main.kt
@@ -4,11 +4,30 @@ fun main() {
     val nanda = Funcionario(
         nome = "Fernanda",
         cpf = "111.222.333-45",
-        salario = 100.00
+        salario = 100.00,
     )
 
     println("Nome ${nanda.nome}")
+    println("Salário ${nanda.salario}")
     println("Bonificação ${nanda.bonificacao()}")
+
+    val Ana = Gerente(
+        nome = "Ana",
+        cpf = "111.222.333-45",
+        salario = 200.00,
+        senha = 123
+    )
+
+    println("Nome ${Ana.nome}")
+    println("Salário ${Ana.salario}")
+    println("Bonificação ${Ana.bonificacao()}")
+
+    if (Ana.autentica(123)) {
+        println("Autentecação confirmada")
+    } else {
+        println("Erro na autenticação")
+    }
+
 }
 
 
