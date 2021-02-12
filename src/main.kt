2c388d4c5c7a2cab5f44bb43673b11b2c536ceaa
Fernanda Alves <fernandaalvesmonteiro7@gmail.com>
2021-02-12 17:03:29 -0300

    Ajustes e correções relacionados a herança

--- src/main.kt
+++ src/main.kt
@@ -1,28 +1,40 @@
 fun main() {
     println("Bem Vindo ao Bytebank")
 
-    val nanda = Funcionario(
+    val funcionario = Funcionario(
         nome = "Fernanda",
         cpf = "111.222.333-45",
         salario = 100.00,
     )
 
-    println("Nome ${nanda.nome}")
-    println("Salário ${nanda.salario}")
-    println("Bonificação ${nanda.bonificacao()}")
+    println("Nome ${funcionario.nome}")
+    println("Salário ${funcionario.salario}")
+    println("Bonificação ${funcionario.bonificacao()}")
 
-    val Ana = Gerente(
+    val gerente = Gerente(
         nome = "Ana",
         cpf = "111.222.333-45",
         salario = 200.00,
         senha = 123
     )
 
-    println("Nome ${Ana.nome}")
-    println("Salário ${Ana.salario}")
-    println("Bonificação ${Ana.bonificacao()}")
+    println("Nome ${gerente.nome}")
+    println("Salário ${gerente.salario}")
+    println("Bonificação ${gerente.bonificacao()}")
 
-    if (Ana.autentica(123)) {
+    val diretor = Diretor(
+        nome = "Ana",
+        cpf = "111.222.333-45",
+        salario = 200.00,
+        senha = 123,
+        plr = 20.0
+    )
+
+    println("Nome ${diretor.nome}")
+    println("Salário ${diretor.salario}")
+    println("Bonificação ${diretor.bonificacao()}")
+
+    if (gerente.autentica(123)) {
         println("Autentecação confirmada")
     } else {
         println("Erro na autenticação")
