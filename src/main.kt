244bdd756064bf3af9e07f70432c23b8992e36c3
Fernanda Alves <fernandaalvesmonteiro7@gmail.com>
2021-02-11 23:26:11 -0300

    Implementando codigo na classe funcionario

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,14 @@
 fun main() {
     println("Bem Vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val nanda = Funcionario(
+        nome = "Fernanda",
+        cpf = "111.222.333-45",
+        salario = 100.00
+    )
 
+    println("Nome ${nanda.nome}")
+    println("Bonificação ${nanda.bonificacao()}")
 }
 
 
