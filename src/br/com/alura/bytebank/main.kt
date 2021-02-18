c9a2418d119cf710cf94c8f7652ef8b6c537e9f3
Fernanda Alves <fernandaalvesmonteiro7@gmail.com>
2021-02-17 21:35:30 -0300

    utilizando variáveis globais

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,6 +1,16 @@
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.totalContas
+
+
 fun main() {
 
+
+    val nanda = Cliente(nome = "Nanda", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = nanda, numeroConta = 1111)
     testaContasDiferentes()
+
+    println("Total conta: $totalContas")
 }
 
 
--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,12 +1,19 @@
 package br.com.alura.bytebank.modelo
 
+var totalContas = 0
+    private set
+
 abstract class Conta(
-    var titular:Cliente,
+    var titular: Cliente,
     val numeroConta: Int
 ) {
 
     var saldo = 0.0
         protected set
+
+    init {
+        totalContas++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -23,6 +30,50 @@ abstract class Conta(
             return true
         } else {
             return false
+        }
+    }
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numeroConta: Int
+) : Conta(
+    titular = titular,
+    numeroConta = numeroConta
+) {
+
+    init {
+        totalContas++
+    }
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if (this.saldo >= valorComTaxa) {
+            this.saldo -= valorComTaxa
+        } else {
+            System.out.println("Saldo insuficiente para realizar operação")
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numeroConta: Int
+) : Conta(
+    titular = titular,
+    numeroConta = numeroConta
+
+){
+
+    init {
+        totalContas++
+    }
+
+    override fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        } else {
+            System.out.println("Saldo insuficiente para realizar operação")
         }
     }
 }
